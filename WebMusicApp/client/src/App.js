3a84e6c2f93bf977c0d43990fdda4bb58b6255ca
AndrewWy1 <droppet228@gmail.com>
2024-05-18 20:08:50 +0300

    first

---
+++ WebMusicApp/client/src/App.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Login, Home } from './components';
+import { app } from "./config/firebase.cofig";
+import { getAuth, } from "firebase/auth";
+import { AnimatePresence } from "framer-motion";
+
+const App = () => {
+  const firebaseAuth = getAuth(app);
+  const navigate = useNavigate();
+  // const [{ user, allSongs, song, isSongPlaying, miniPlayer }, dispatch] =
+  //   useStateValue();
+  // const [isLoading, setIsLoading] = useState(false);
+
+
+  const [auth, setAuth] = useState(false || window.localStorage.getItem("auth" === "true"));
+
+  useEffect(() => {
+    //setIsLoading(true);
+    firebaseAuth.onAuthStateChanged((userCred) => {
+      if (userCred) {
+        userCred.getIdToken().then((token) => {
+          console.log(token);
+          //window.localStorage.setItem("auth", "true");
+          // validateUser(token).then((data) => {
+          //   // dispatch({
+          //   //   type: actionType.SET_USER,
+          //   //   user: data,
+          //   // });
+          // });
+        });
+        //setIsLoading(false);
+      } else {
+        setAuth(false);
+        // dispatch({
+        //   type: actionType.SET_USER,
+        //   user: null,
+        // });
+        //setIsLoading(false);
+        window.localStorage.setItem("auth", "false");
+        navigate("/login");
+      }
+    });
+  }, []);
+
+  return (
+    <AnimatePresence mode='wait'>
+      <div className="h-auto min-w-[680px] bg-primary flex items-center justify-center">
+        <Routes>
+          <Route path='/login' element={<Login setAuth={setAuth} />} />
+          <Route path='/*' element={<Home />} />
+        </Routes>
+      </div>
+    </AnimatePresence>
+  )
+}
+
+export default App
---
+++ WebMusicApp/server/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+require("dotenv/config");
+
+const cors = require("cors");
+const {default : mongoose} = require("mongoose");
+
+// app.use(cors({origin : true}));
+
+
+app.get("/", (req, res) =>{
+
+    return res.json("Hi");
+})
+
+const userRoute = require("./routes/auth");
+app.use("/api/users/", userRoute);
+
+mongoose.connect(process.env.DB_STRING, {useNewUrlParser: true});
+mongoose.connection
+.once("open", () => console.log("Complete connection"))
+.on("error", (error) => {
+    console.log(`ERROR ${error}`)
+})
+
+app.listen(4000, () => console.log("lisitening to port 4000"));
